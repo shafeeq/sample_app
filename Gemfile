7f52574fd75cb8103f653d0ebfa2371cd3ad12a9
Shafeeq K <shafeeq94@gmail.com>
2013-05-09 23:18:32 +0530

    Add Guard & Spork. Home page with basetitle

--- Gemfile
+++ Gemfile
@@ -4,8 +4,10 @@ gem 'rails', '3.2.13'
 
 group :development, :test do
       gem 'sqlite3', '1.3.5'
-        gem 'rspec-rails', '2.11.0'
+      gem 'rspec-rails', '2.11.0'
+      gem 'guard-rspec', '1.2.1'
 end
+
 
 # Gems used only for assets and not required
 # in production environments by default.
@@ -19,6 +21,10 @@ gem 'jquery-rails', '2.0.2'
 
 group :test do
   gem 'capybara', '1.1.2'
+  gem 'rb-inotify', '0.8.8'
+  gem 'libnotify', '0.5.9'
+  gem 'guard-spork', '1.2.0'
+  gem 'spork', '0.9.2'
 end
 
 group :production do
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,11 +7,17 @@ describe "Static pages" do
             visit '/static_pages/home'
             page.should have_selector('h1', :text => "Sample App")
         end
-        it "should have the title Home" do
+        it "should have the base title" do
             visit '/static_pages/home'
-            page.should have_selector('title', :text => "#{base_title} | Home")
+            page.should have_selector('title',
+                        :text => "Ruby on Rails Tutorial Sample App")
         end
     end
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
+    end
+
     describe "Help page" do
         it "should have the h1 'Help'" do
             visit '/static_pages/help'
@@ -22,6 +28,8 @@ describe "Static pages" do
             page.should have_selector('title', :text => "#{base_title} | Help")
         end
     end
+
+
     describe "About page" do
         it "should have the h1 'About'" do
             visit '/static_pages/about'
@@ -32,6 +40,8 @@ describe "Static pages" do
             page.should have_selector('title', :text => "#{base_title} | About")
         end
     end
+
+
     describe "Contact page" do
         it "should have the h1 'Contact'" do
             visit '/static_pages/contact'
