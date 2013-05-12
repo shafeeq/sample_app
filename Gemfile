df7a16188ca110948edc2dfc5a1010ee4935d814
Shafeeq K <shafeeq94@gmail.com>
2013-05-12 12:08:39 +0530

    Finish user signup

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ group :test do
   gem 'libnotify', '0.5.9'
   gem 'guard-spork', '1.2.0'
   gem 'spork', '0.9.2'
+  gem 'factory_girl_rails' , '4.1.0'
 end
 
 group :production do
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+    factory :user do
+        name "Shafeeq K"
+        email "shafeeq@example.com"
+        password "foobar"
+        password_confirmation "foobar"
+    end
+end
