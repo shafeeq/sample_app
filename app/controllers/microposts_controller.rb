400b105d9a1cfabdd24596be91cdad1e55af410b
Shafeeq K <shafeeq94@gmail.com>
2013-05-14 18:34:38 +0530

    Add user following

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -18,9 +18,19 @@ class MicropostsController < ApplicationController
 		@micropost.destroy
 		redirect_to root_url
 	end
+	def wrap(content)
+    sanitize(raw(content.split.map{ |s| wrap_long_string(s) }.join(' ')))
+  end
 	private
 	def correct_user
 		@micropost = current_user.microposts.find_by_id(params[:id])
 		redirect_to root_url if @micropost.nil?
+	end
+	def wrap_long_string(text, max_width = 30)
+      zero_width_space = "&#8203;"
+      regex = /.{1,#{max_width}}/
+      (text.length < max_width) ? text : 
+                                  text.scan(regex).join(zero_width_space)
+    end
 
 end
